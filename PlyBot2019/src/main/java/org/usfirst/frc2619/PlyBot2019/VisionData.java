facda0a2d94be5f27b942fa56219455b12edddad
Nathaniel Kalantar <NathanielK392@gmail.com>
2019-02-02 17:47:54 -0500

    Started working on vision calculations for the robot

--- PlyBot2019/src/main/java/org/usfirst/frc2619/PlyBot2019/VisionData.java
+++ PlyBot2019/src/main/java/org/usfirst/frc2619/PlyBot2019/VisionData.java
@@ -4,14 +4,19 @@ import java.util.ArrayList;
 
 public class VisionData {
 
-    private static final int TIMESTAMP_ID = 0;
-    private static final String SPLIT_CHAR = ":";
+    private static final String TIMESTAMP_ID = "TIME";
+    private static final String TARGET_ID = "TARGET";
+    private static final String PAIR_ID = "PAIR";
+    private static final String MSG_SPLIT_CHAR = ":";
+    private static final String DATA_SPLIT_CHAR = ",";
+
 
     private double timestamp;
     private ArrayList<Target> targets;
 
-    public VisionData() {
-
+    public VisionData(double timestamp, ArrayList<Target> targets) {
+        this.timestamp = timestamp;
+        this.targets = targets;
     }
 
     /**
@@ -20,24 +25,107 @@ public class VisionData {
      * @return a new VisionData object
      */
     public static VisionData parseMessage(String msg) {
-        String[] values = msg.split(SPLIT_CHAR);
+        String[] msgData = msg.split(MSG_SPLIT_CHAR);
+
+        double camTimestamp = 0.0;
+        ArrayList<Target> camTargets = new ArrayList<Target>();
         
-        for (String targetInfo : values) {
-            if 
+        for (String values : msgData) {
+            String[] data = values.split(DATA_SPLIT_CHAR);
+            String label = data[0];
+
+            if (label == TIMESTAMP_ID)
+                camTimestamp = Double.parseDouble(data[1]);
+
+            else if (label == TARGET_ID) {
+                int x = Integer.parseInt(data[1]);
+                int y = Integer.parseInt(data[2]);
+                Target tempTarget = new Target(x, y);
+
+                camTargets.add(tempTarget);
+            }
+
+            else if (label == PAIR_ID) {
+                int xLeft = Integer.parseInt(data[1]);
+                int yLeft = Integer.parseInt(data[2]);
+                int xRight = Integer.parseInt(data[3]);
+                int yRight = Integer.parseInt(data[4]);
+                Target tempTarget = new TargetPair(xLeft, yLeft, xRight, yRight);
+
+                camTargets.add(tempTarget);
+            }
         }
 
-        return new VisionData();
+        VisionData tempVisionData = new VisionData(camTimestamp, camTargets);
+        return tempVisionData;
     }
 
-    private class Target {
+    /**
+     * A target object to represent the reflective tape coordinates.
+     * Instances of this super class represent single pieces of angled tape.
+     */
+    public static class Target {
 
-        private int x_mid;
-        private int y_mid;
+        private int x;
+        private int y;
 
-        public Target(int x_mid, int y_mid) {
-            this.x_mid = x_mid;
-            this.y_mid = y_mid;
+        /**
+         * @param x The x value of the target's center
+         * @param y The y value of the target's center
+         */
+        public Target(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
 
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+    }
+
+    /**
+     * TargetPair represents a pair of angled tape that outline a port.
+     * Coordinates of each piece of tape, as well as the center of the two, are stored.
+     */
+    public static class TargetPair extends Target {
+
+        private int xLeft;
+        private int yLeft;
+        private int xRight;
+        private int yRight;
+
+        /**
+         * @param xLeft The x value of the left target
+         * @param yLeft The y value of the left target
+         * @param xRight The x value of the right target
+         * @param yRight the y value of the right target
+         */
+        public TargetPair(int xLeft, int yLeft, int xRight, int yRight) {
+            this.xLeft = xLeft;
+            this.yLeft = yLeft;
+            this.xRight = xRight;
+            this.yRight = yRight;
+        }
+
+        public int getXLeft() {
+            return xLeft;
+        }
+
+        public int getYLeft() {
+            return yLeft;
+        }
+
+        public int getXRight() {
+            return xRight;
+        }
+
+        public int getYRight() {
+            return yRight;
+        }
     }
 }
--- PlyBot2019/src/main/java/org/usfirst/frc2619/PlyBot2019/VisionUtil.java
+++ PlyBot2019/src/main/java/org/usfirst/frc2619/PlyBot2019/VisionUtil.java
@@ -2,10 +2,11 @@ package org.usfirst.frc2619.PlyBot2019;
 
 import edu.wpi.first.wpilibj.SerialPort;
 import org.usfirst.frc2619.PlyBot2019.VisionData;
+import org.usfirst.frc2619.PlyBot2019.VisionData.Target;
 
 public class VisionUtil {
     
-    //SerialPort constants
+    // SerialPort constants
     public static final int DEFAULT_BAUD_RATE = 115200;
     public static final SerialPort.Port DEFAULT_SERIAL_PORT = SerialPort.Port.kUSB;
 
@@ -13,6 +14,16 @@ public class VisionUtil {
     public static final int CAM_WIDTH = 320;
     public static final int CAM_HEIGHT = 240;
     public static final int CAM_FPS = 60;
+
+    // Calculation constats
+    public static final int CENTER_X = (int)(CAM_WIDTH / 2);
+    public static final int CENTER_Y = (int)(CAM_HEIGHT / 2);
+
+    public static final double FOV_H = 65;
+    public static final double FOV_V = 49.75;
+
+    public static final double FOCAL_LENGTH_H = CAM_WIDTH / (2 * Math.tan(FOV_H / 2));
+    public static final double FOCAL_LENGTH_V = CAM_HEIGHT / (2 * Math.tan(FOV_V / 2));
 
     /**
      * Creates a SerialPort object with the default Baud Rate and Port
@@ -72,8 +83,95 @@ public class VisionUtil {
      * @return a VisionData object
      */
     public static VisionData parseMessage(String msg) {
+        return VisionData.parseMessage(msg);
+    }
 
-        return new VisionData();
+    /**
+     * Calculates horizontal angle from center to a coordinate.
+     * 
+     * @param x x coordinate
+     * @return horizontal angle from center to a coordinate
+     */
+    public static double calculateYaw(int x) {
+        return Math.atan( (CENTER_X - x) / FOCAL_LENGTH_H );
+    }
+
+    public static int calculate
+
+    /**
+     * Calculates vertical angle from center to a coordinate.
+     * 
+     * @param y coordinate
+     * @return vertical angle from center to a coordinate
+     */
+    public static double calculatePitch(int y) {
+        return Math.atan( (CENTER_Y - y) / FOCAL_LENGTH_V );
+    }
+
+    /**
+     * Calculates the distance from the camera directly to the target coordinates
+     * 
+     * @param x The x coordinate of the point
+     * @param y The y coordinate of the point
+     * @param targetHeight The height of the target
+     * @return Range from camera to target
+     */
+    public static double calculateRange(int x, int y, double targetHeight) {
+        double height = targetHeight - getCamHeight();
+        double yaw = calculateYaw(x);
+        double pitch = calculatePitch(y);
+
+        double range = Math.sqrt( Math.pow((height/Math.sin(pitch)), 2) + (Math.pow((height * Math.tan(yaw) / Math.tan(pitch)), 2)) );
+        return range;
+    }
+
+    /**
+     * Calculates the distance from the camera directly to the target
+     * 
+     * @param target The target to find the range to
+     * @param targetHeight The height of the target
+     * @return Range from the robot to the target
+     */
+    public static double calculateRange(Target target, double targetHeight) {
+        int x = target.getX();
+        int y = target.getY();
+
+        return calculateRange(x, y, targetHeight);
+    }
+
+    /**
+     * Calculates the ground distance from the robot to the target coordinates
+     * 
+     * @param x The x coordinate of the point
+     * @param y The y coordinate of the point
+     * @param targetHeight The height of the target
+     * @return Ground distance from the robot to the target
+     */
+    public static double calculateHorizontalDistance(int x, int y, double targetHeight) {
+        double height = targetHeight - getCamHeight();
+        double yaw = calculateYaw(x);
+        double pitch = calculatePitch(y);
+
+        double dist = Math.sqrt( Math.pow((height/Math.tan(pitch)), 2) + (Math.pow((height * Math.tan(yaw) / Math.tan(pitch)), 2)) );
+        return dist;
+    }
+
+    /**
+     * Calculates the ground distance from the robot to the target
+     * 
+     * @param target The target to find the distance to
+     * @param targetHeight The height of the target
+     * @return Ground distance from the robot to the target
+     */
+    public static double calculateHorizontalDistance(Target target, double targetHeight) {
+        int x = target.getX();
+        int y = target.getY();
+
+        return calculateHorizontalDistance(x, y, targetHeight);
+    }
+
+    public static double getCamHeight() {
+        return 5.0;
     }
 
 }
