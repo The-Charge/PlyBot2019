913f56e76fe19de7163032a4ee21f12f92c44db4
TylerMBarton <tylebart20@midlandps.org>
2019-03-02 09:38:14 -0500

    Added isFinished future logic and end code

--- PlyBot2019/src/main/java/org/usfirst/frc2619/PlyBot2019/commands/LineDiscover.java
+++ PlyBot2019/src/main/java/org/usfirst/frc2619/PlyBot2019/commands/LineDiscover.java
@@ -128,11 +128,14 @@ public class LineDiscover extends Command {
     @Override
     protected boolean isFinished() {
         return false;
+        //return Robot.driveTrain.getCurrentAmps() > 4;
     }
 
     // Called once after isFinished returns true
     @Override
     protected void end() {
+        Robot.driveTrain.stop();
+        Robot.driveTrain.setPercentVBus();
     }
 
     // Called when another command which requires one or more of the same
